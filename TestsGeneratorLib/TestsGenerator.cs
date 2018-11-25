468a049454443f4d4ef7267f672586c74ee1c19e
nnFrolovnn <mfi.ge.vlad@gmail.com>
2018-11-25 16:53:42 +0300

    create ctor

--- TestsGeneratorLib/TestsGenerator.cs
+++ TestsGeneratorLib/TestsGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using TestsGeneratorLib.IO;
 
 namespace TestsGeneratorLib
@@ -12,26 +13,29 @@ namespace TestsGeneratorLib
         private int maxReadersCount;
         private int maxGeneratorsCount;
         private int maxWritersCount;
+        private string outputDirectory;
         private ITestWriter writer;
         private ITestReader reader;
 
-        public TestsGenerator(int maxReadersCount, int maxGeneratorsCount, int maxWritersCount)
+        public TestsGenerator(int maxReadersCount, int maxGeneratorsCount, int maxWritersCount, string outputDir)
         {
             this.maxGeneratorsCount = maxGeneratorsCount;
             this.maxReadersCount = maxReadersCount;
             this.maxWritersCount = maxWritersCount;
             writer = new TestWriter("DefaultOUT");
             reader = new TestReader();
+            outputDirectory = outputDir;
         }
 
         public TestsGenerator(int maxReadersCount, int maxGeneratorsCount, 
-                              int maxWritersCount, ITestWriter writer, ITestReader reader)
+                              int maxWritersCount, ITestWriter writer, ITestReader reader, string outputDir)
         {
             this.maxGeneratorsCount = maxGeneratorsCount;
             this.maxReadersCount = maxReadersCount;
             this.maxWritersCount = maxWritersCount;
             this.writer = writer;
             this.reader = reader;
+            outputDirectory = outputDir;
         }
 
         public Task Generate(List<string> files)

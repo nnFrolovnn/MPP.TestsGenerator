8dec2c992acaa8c030b0fca1ccca7ac43af3118b
nnFrolovnn <mfi.ge.vlad@gmail.com>
2018-11-26 23:17:22 +0300

    add app

---
+++ GeneratorApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestsGeneratorLib.TestsGenerator generator = new TestsGeneratorLib.TestsGenerator(2, 1, 1, "outTests");
+
+            generator.Generate(new List<string>()
+            {
+                "1.txt",
+                "2.txt",
+                "3.txt"
+            }).Wait();
+
+            Console.WriteLine("end...");
+            Console.ReadKey();
+        }
+    }
+}
